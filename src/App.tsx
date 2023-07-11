95c4acfda8e0fabe43a5cef68d5a55a9f08c6485
Γιάννης Χατζηδημητρίου <jxatz@eurodyn.com>
2023-07-11 16:37:27 +0300

    Highlight the selected genre

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ function App() {
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
           <GenreList
+            selectedGenre={selectedGenre}
             onSelectGenre={(genre) => {
               setSelectedGenre(genre);
             }}
