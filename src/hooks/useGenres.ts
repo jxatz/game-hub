af753bbfd7f3af0b0a0ea0f9477ecb8e1485b709
Γιάννης Χατζηδημητρίου <jxatz@eurodyn.com>
2023-10-25 15:28:26 +0300

    Ship genres with app

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,12 +1,14 @@
-import useData from "./useData";
-
+//import useData from "./useData";
+import genres from "../data/genres";
 interface Genre {
   id: number;
   name: string;
   image_background: string;
 }
+//substitute the remote call for genres with static data
+//const useGenres = () => useData<Genre>("/genres");
 
-const useGenres = () => useData<Genre>("/genres");
+const useGenres = () => ({ data: genres, error: null, isLoading: false });
 
 export default useGenres;
 export type { Genre };
